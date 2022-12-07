5157d390e69edc767fec95664c5d2bc537ff76e2
Josh <joshuawen97@gmail.com>
2022-12-07 16:30:24 +0700

    gql added, error with toaster

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -4,7 +4,11 @@ import Avatar from './Avatar';
 import { LinkIcon, PhotographIcon } from '@heroicons/react/outline';
 import { useForm } from 'react-hook-form';
 import { Button } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client';
+import { ADD_POST, ADD_SUBREDDIT } from '../graphql/mutations';
+import client from '../apollo-client';
+import { GET_SUBREDDIT_BY_TOPIC } from '../graphql/queries';
+import toast from 'react-hot-toast';
 type FormData = {
 	postTitle: string;
 	postBody: string;
@@ -14,6 +18,10 @@ type FormData = {
 
 function PostBox() {
 	const { data: session } = useSession();
+	//
+	const [addPost] = useMutation(ADD_POST);
+	const [addSubreddit] = useMutation(ADD_SUBREDDIT);
+	//
 	const [imageBoxOpen, setImageBoxOpen] = useState<boolean>(false);
 	const {
 		register,
@@ -22,8 +30,82 @@ function PostBox() {
 		watch,
 		formState: { errors },
 	} = useForm<FormData>();
+
+	const onSubmit = handleSubmit(async (formData) => {
+		console.log(formData);
+		const notification = toast.loading('Creating new post...');
+		try {
+			// query for subreddit topic
+			const {
+				data: { getSubredditListByTopic },
+			} = await client.query({
+				query: GET_SUBREDDIT_BY_TOPIC,
+				variables: {
+					topic: formData.subreddit,
+				},
+			});
+			const subredditExists = getSubredditListByTopic.length > 0;
+
+			if (!subredditExists) {
+				// create subreddit
+				console.log('subreddit is new creating a new subreddit!');
+				const {
+					data: { insertSubreddit: newSubreddit },
+				} = await addSubreddit({
+					variables: {
+						topic: formData.subreddit,
+					},
+				});
+				console.log('Creating post...', formData);
+				const image = formData.postImage || '';
+
+				const {
+					data: { insertPost: newPost },
+				} = await addPost({
+					variables: {
+						body: formData.postBody,
+						image: image,
+						subreddit_id: newSubreddit.id,
+						title: formData.postTitle,
+						username: session?.user?.name,
+					},
+				});
+				console.log('New post added:', newPost);
+			} else {
+				// use existing
+				const image = formData.postImage || '';
+				const {
+					data: { insertPost: newPost },
+				} = await addPost({
+					variables: {
+						body: formData.postBody,
+						image: image,
+						subreddit_id: getSubredditListByTopic[0].id,
+						title: formData.postTitle,
+						username: session?.user?.name,
+					},
+				});
+				console.log('New post added', newPost);
+			}
+
+			setValue('postBody', '');
+			setValue('postImage', '');
+			setValue('postTitle', '');
+			setValue('subreddit', '');
+			toast.success('New Post Created!', {
+				id: notification,
+			});
+		} catch (error) {
+			toast.error('Whoops something went wrong!', {
+				id: notification,
+			});
+		}
+	});
 	return (
-		<form className="sticky top-16 z-50 bg-white border rounded-md border-gray-300 p-2">
+		<form
+			onSubmit={onSubmit}
+			className="sticky top-16 z-50 bg-white border rounded-md border-gray-300 p-2"
+		>
 			<div className="flex items-center space-x-3">
 				{/* avatar */}
 				<Avatar />
@@ -88,7 +170,10 @@ function PostBox() {
 						</div>
 					)}
 					{!!watch('postTitle') && (
-						<button className="w-full rounded-full bg-blue-400 text-white">
+						<button
+							type="submit"
+							className="w-full rounded-full bg-blue-400 text-white"
+						>
 							Create Post
 						</button>
 					)}
